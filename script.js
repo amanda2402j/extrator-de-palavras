ab176ceb7aa5bb3f09eea7c3dfa4b828e632e1a2
amanda2402j <amanda.buryca@escola.pr.gov.br>
2025-09-16 16:58:37 -0300

    16/09

--- script.js
+++ script.js
@@ -8,4 +8,34 @@ function mostrapalavrachave(){
     const palavras = texto.split("");
     
     compoResultado.textContent = palavras.join(",");
+
+}
+function processatexto(texto){
+    let palavras = texto.split (/\P {l})+/u);
+    const frequencias = contafrequencia(palavras);
+    let ordenadas = Object.keys(fequencias).sort(ordenapalavra);
+
+    function ordenapalavra(p1,p2){
+        return fequencias[p2]-frequencias[p1];
+    }
+
+    console.log(ordenadas);
+    return ordenadas.slice(0,10);
+
+}
+function contafrequencia(palavras){
+    let frequencias ={}; 
+    for( let i of palavras){
+        frequencias[i]=0;
+        for (let j of palavras){
+            if (i==j){
+                frequencias[i]++;
+                
+            }
+        }
+    }
+}
+
+
+    return palavras;
 }
